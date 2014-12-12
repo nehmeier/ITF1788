02b19dc36eaf1c76d181389ecd7ca3dbc4d7405c
Maximilian Kiesner <maximilian.kiesner@stud-mail.uni-wuerzburg.de>
2014-12-12 14:33:42 +0100

    Fixed issue with relative paths

--- defaultGenerator.sh
+++ defaultGenerator.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
-ITF1788_HOME=$(pwd)
-cd ${ITF1788_HOME}/src
+ITF1788_HOME="$(dirname "${BASH_SOURCE[0]}")"
+# if $ITF1788_HOME does not start with '/', concat with current path
+if [[ ! "$ITF1788_HOME" = /* ]]; then
+    ITF1788_HOME="$(pwd)/$ITF1788_HOME"
+fi
+
+cd "${ITF1788_HOME}/src"
 
 if [ ! $@ ]; then
-    python3 main.py -s "${ITF1788_HOME}/ITL files/" -o "${ITF1788_HOME}/output/"
+    python3 $ITF1788_HOME/src/main.py -s "${ITF1788_HOME}/ITL files/" -o "${ITF1788_HOME}/output/"
 else
     python3 main.py $@
 fi
