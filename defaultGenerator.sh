a188b833545b603f3fb5cb6f644295ddea4d7f3f
Maximilian Kiesner <maximilian.kiesner@stud-mail.uni-wuerzburg.de>
2014-12-15 00:59:03 +0100

    Default generator now supports spaces in path
    Previously, if the path name of the the cloned repository contained any spaces
    or other strange characters, the script may not have worked if called
    from another location.

--- defaultGenerator.sh
+++ defaultGenerator.sh
@@ -1,15 +1,10 @@
 #!/usr/bin/env bash
 
-ITF1788_HOME="$(dirname "${BASH_SOURCE[0]}")"
-# if $ITF1788_HOME does not start with '/', concat with current path
-if [[ ! "$ITF1788_HOME" = /* ]]; then
-    ITF1788_HOME="$(pwd)/$ITF1788_HOME"
-fi
-
+ITF1788_HOME="$( cd "$( dirname "$0" )" && pwd )"
 cd "${ITF1788_HOME}/src"
 
 if [ ! $@ ]; then
-    python3 $ITF1788_HOME/src/main.py -s "${ITF1788_HOME}/ITL files/" -o "${ITF1788_HOME}/output/"
+    python3 "$ITF1788_HOME/src/main.py" -s "${ITF1788_HOME}/ITL files/" -o "${ITF1788_HOME}/output/"
 else
     python3 main.py $@
 fi
